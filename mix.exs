e7300c2c24611b46a72563745ce9afc2c6262fd3
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-04-14 14:39:18 -0700

    Bump alice version

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule DrunkenSavantsAlice.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:alice, "~> 0.4.2", override: true},
+      {:alice, "~> 0.4.3", override: true},
       {:alice_roller,       "~> 0.3.1"},
       {:alice_good_morning, "~> 1.0"},
       {:alice_karma,        "~> 0.3"}
